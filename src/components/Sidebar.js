0ad55f0dca7628a86514678ca4f7ee5fc5fe9d7c
Tobias Hochgürtel <tobias.hochguertel@valtech.de>
2018-02-11 15:34:20 +0100

    Add C4Person Notation

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,6 +4,7 @@ import {C4Container} from "../notation/C4Container";
 import {C4Database} from "../notation/C4Database";
 import {C4DeploymentNode} from "../notation/C4DeploymentNode";
 import {C4ExecutionEnvironment} from "../notation/C4ExecutionEnvironment";
+import {C4Person} from "../notation/C4Person";
 
 export class Sidebar {
 
@@ -27,10 +28,14 @@ export class Sidebar {
         let c4ExecutionEnvironment = new C4ExecutionEnvironment();
         C4utils.registCodec(c4ExecutionEnvironment);
 
+        let c4Person = new C4Person();
+        C4utils.registCodec(c4Person);
+
+
         // Adds custom sidebar entry
         ui.sidebar.addPalette(sidebar_id, sidebar_title, true, function (content) {
             // let verticies = [C4Person, C4SoftwareSystem, C4Container, C4Component, C4ExecutionEnvironment, C4DeploymentNode, C4Database];
-            let verticies = [c4Component, c4Container, c4Database, c4DeploymentNode, c4ExecutionEnvironment];
+            let verticies = [c4Component, c4Container, c4Database, c4DeploymentNode, c4ExecutionEnvironment, c4Person];
             for (let i in verticies) {
                 let c4notationMxCell = verticies[i].mxcell;
                 content.appendChild(
--- src/notation/C4Person.js
+++ src/notation/C4Person.js
@@ -1,43 +1,62 @@
-export class C4Person {
+import {C4Notation} from "./C4Notation";
+import {C4utils} from "../utilities/C4utils";
 
-    create() {
-        let group = new mxCell(
-            ''
-            , new mxGeometry(0, 0, 160, 180)
-            , 'group;rounded=0;labelBackgroundColor=none;fillColor=none;fontColor=#ffffff;align=center;html=1;'
-        );
+export class C4Person extends C4Notation {
+
+    constructor() {
+        let dimension = {
+            x: 0,
+            y: 0,
+            width: 160,
+            height: 180
+        };
+        let style = '';
+        let label = '';
+
+        super(dimension, style, label);
+    }
+
+    init() {
+        let group = new mxCell('', C4utils.createMxGeometry(this.dimension), 'group;rounded=0;labelBackgroundColor=none;fillColor=none;fontColor=#ffffff;align=center;html=1;');
         group.setVertex(true);
         group.setConnectable(false);
-        group.setAttribute('c4Type', 'person');
-        group.c4 = this;
-        let body = new mxCell(
-            ''
-            , new mxGeometry(0, 70, 160, 110)
-            , 'rounded=1;whiteSpace=wrap;html=1;labelBackgroundColor=none;fillColor=#08427b;fontColor=#ffffff;align=center;arcSize=33;strokeColor=#3c7fc0;'
-        );
+        group.setAttribute('c4Type', this.constructor.name);
+        group.c4 = true;
+
+        let body = new mxCell('', C4utils.createMxGeometry({
+            x: 0,
+            y: 70,
+            width: 160,
+            height: 110
+        }), "rounded=1;whiteSpace=wrap;html=1;labelBackgroundColor=none;fillColor=#08427b;fontColor=#ffffff;align=center;arcSize=33;strokeColor=#3c7fc0;");
+
         body.setParent(group);
         body.setVertex(true);
         body.setValue(mxUtils.createXmlDocument().createElement('object'));
-        body.setAttribute('label', 'name<div>[Person]</div><div><br></div><div>Beschreibung</div>');
+
+        body.setAttribute('label', "name<div>[Person]</div><div><br></div><div>Beschreibung</div>");
         body.setAttribute('placeholders', '1');
         body.setAttribute('c4Name', 'name');
         body.setAttribute('c4Type', 'body');
         body.setAttribute('c4Description', 'Beschreibung');
-        body.c4 = this;
-        var head = new mxCell(
-            ''
-            , new mxGeometry(40, 0, 80, 80)
-            , 'rounded=1;whiteSpace=wrap;html=1;aspect=fixed;labelBackgroundColor=none;fillColor=#08427b;fontSize=14;font-weight:bold;fontColor=#ffffff;align=center;strokeColor=#3c7fc0;ellipse;'
-        );
+
+
+        let head = new mxCell('', C4utils.createMxGeometry({
+            x: 40,
+            y: 0,
+            width: 80,
+            height: 80
+        }), "rounded=1;whiteSpace=wrap;html=1;aspect=fixed;labelBackgroundColor=none;fillColor=#08427b;fontSize=14;font-weight:bold;fontColor=#ffffff;align=center;strokeColor=#3c7fc0;ellipse;");
+
         head.setParent(group);
         head.setVertex(true);
         head.setAttribute('c4Type', 'head');
-        head.c4 = this;
+        head.c4 = true;
+
         group.insert(head);
         group.insert(body); // child: 0 !!
-        return group;
+
+        this.mxcell = group;
     }
 
 }
-
-// c4Utils.registCodec(C4Person);
