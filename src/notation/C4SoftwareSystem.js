fc06de1d66c0a221ee2d38c426e804ee6d4018f8
Tobias Hochgürtel <tobias.hochguertel@valtech.de>
2018-02-18 18:11:05 +0100

    Refactors to Style class.

--- src/notation/C4SoftwareSystem.js
+++ src/notation/C4SoftwareSystem.js
@@ -1,4 +1,5 @@
 import {C4Notation} from "./C4Notation";
+import {C4SoftwareSystemStyle} from "./C4SoftwareSystemStyle";
 
 export class C4SoftwareSystem extends C4Notation {
 
@@ -9,10 +10,9 @@ export class C4SoftwareSystem extends C4Notation {
             width: 160,
             height: 110
         };
-        let style = 'rounded=1;whiteSpace=wrap;html=1;labelBackgroundColor=none;fillColor=#1168bd;fontSize=14;font-weight:bold;fontColor=#ffffff;align=center;arcSize=7;strokeColor=#3c7fc0;';
         let label = 'name<div>[Software System]</div><div><br></div><div>Beschreibung</div>';
-
-        super('', dimension, style, label);
+        let c4Style = new C4SoftwareSystemStyle();
+        super('', dimension, c4Style.toString(), label);
     }
 
 }
---
+++ src/notation/C4SoftwareSystemStyle.js
@@ -0,0 +1,5 @@
+import {C4AbstractionStyle} from "./C4AbstractionStyle";
+
+export class C4SoftwareSystemStyle extends C4AbstractionStyle {
+    fillColor = "#1168bd";
+}
