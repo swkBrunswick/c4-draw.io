0cff02934e0b294c12848b1069686036837b3dbe
Tobias Hochgürtel <tobias.hochguertel@valtech.de>
2018-02-18 17:28:29 +0100

    Add Style for C4 Infrastructure Notation.

---
+++ src/components/C4InfrastructureStyle.js
@@ -0,0 +1,10 @@
+import {C4Style} from "./C4Style";
+
+export class C4InfrastructureStyle extends C4Style {
+    fillColor = "#ffffff";
+    fontColor = "#000000";
+    align = "left";
+    arcSize = 3;
+    strokeColor = "#000000";
+    verticalAlign = "bottom";
+}
--- src/notation/C4ExecutionEnvironment.js
+++ src/notation/C4ExecutionEnvironment.js
@@ -1,4 +1,5 @@
 import {C4Notation} from "./C4Notation";
+import {C4InfrastructureStyle} from "../components/C4InfrastructureStyle";
 
 export class C4ExecutionEnvironment extends C4Notation {
 
@@ -9,8 +10,10 @@ export class C4ExecutionEnvironment extends C4Notation {
             width: 240,
             height: 230
         };
-        let style = 'rounded=1;whiteSpace=wrap;html=1;labelBackgroundColor=none;fillColor=#ffffff;fontSize=14;font-weight:bold;fontColor=#ffffff;align=left;arcSize=3;strokeColor=#000000;verticalAlign=bottom;';
         let label = '<div style="text-align: left">hostname</div><div style="text-align: left">[operationSystem]</div><div style="text-align: right">scalingFactor</div>';
+
+        let c4Style = new C4InfrastructureStyle();
+        let style = c4Style.toString();
 
         super('', dimension, style, label);
     }
